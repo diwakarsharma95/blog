b613b230b754c747c86b747b2cd4833783fb0efc
Diwakar Sharma <diwakarsharma95@gmail.com>
2018-12-14 16:11:54 +1000

    Model-Form Binding

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -80,7 +80,11 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        //find the post in the database and save it as a variable
+        $post = Post::find($id);
+
+        //return the view and pass in the variable we previously created
+        return view('posts.edit')->withPost($post);
     }
 
     /**
