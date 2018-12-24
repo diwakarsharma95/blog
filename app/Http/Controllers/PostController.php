dad57590e791979443a88b430b28e5806f02be5a
Diwakar Sharma <diwakarsharma95@gmail.com>
2018-12-24 18:10:00 +1000

    Pagination in Laravel

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post:: orderBy('id','desc')->paginate(1);
         return view('posts.index')->withPosts($posts);
     }
 
